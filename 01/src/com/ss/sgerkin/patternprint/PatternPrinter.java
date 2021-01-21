ccabcab2dff976e62a6babdf5f9aa6f37f3196ed
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-01-20 23:36:35 -0500

    Add method documentation

--- 01/src/com/ss/sgerkin/patternprint/PatternPrinter.java
+++ 01/src/com/ss/sgerkin/patternprint/PatternPrinter.java
@@ -3,16 +3,29 @@ package com.ss.sgerkin.patternprint;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * Class for printing star and dot patterns.
+ */
 public class PatternPrinter {
 
   private final int DOT_START_SIZE = 8;
 
+  /**
+   * Main driver for printing the patterns.
+   *
+   * @param args Unused.
+   */
   public static void main(String[] args) {
     var patternPrinter = new PatternPrinter();
     var pattern = patternPrinter.constructPatterns();
     System.out.println(pattern);
   }
 
+  /**
+   * Constructs all patterns as a single String value.
+   *
+   * @return all the patterns.
+   */
   private String constructPatterns() {
     return IntStream
         .rangeClosed(1, 4)
@@ -20,6 +33,12 @@ public class PatternPrinter {
         .collect(Collectors.joining("\n"));
   }
 
+  /**
+   * Gets a pattern for a specific pattern number.
+   *
+   * @param patternNum the pattern number to get.
+   * @return the constructed pattern.
+   */
   private String getPattern(int patternNum) {
     var sb = new StringBuilder(patternNum + ")\n");
 
@@ -45,6 +64,13 @@ public class PatternPrinter {
     return sb.toString();
   }
 
+  /**
+   * Gets a line of '*' for the pattern.
+   *
+   * @param patternNum the pattern number currently being constructed.
+   * @param line       the line number for the current star pattern.
+   * @return the constructed star line.
+   */
   private String getStarLine(int patternNum, int line) {
     var sb = new StringBuilder("*".repeat(line));
 
@@ -58,14 +84,32 @@ public class PatternPrinter {
     return sb.toString();
   }
 
+  /**
+   * Gets a line of '.' for the pattern.
+   *
+   * @param patternNum the pattern number currently being constructed.
+   * @return the constructed dot line.
+   */
   private String getDotLine(int patternNum) {
     return ".".repeat(DOT_START_SIZE + patternNum);
   }
 
+  /**
+   * Determines if stars should be reversed in pattern (prepended rather than appended).
+   *
+   * @param patternNum the pattern number currently being constructed.
+   * @return true if stars should be prepended rather than appended.
+   */
   private boolean reverseStars(int patternNum) {
     return patternNum % 2 == 0;
   }
 
+  /**
+   * Determines if a line of stars should be indented (and repeated).
+   *
+   * @param patternNum the pattern number currently being constructed.
+   * @return true if stars should be indented.
+   */
   private boolean shouldIndent(int patternNum) {
     return patternNum >= 3;
   }
