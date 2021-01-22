ba6bd5795f664bf15c26b03add9aa4395832bb2d
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-01-22 12:14:36 -0500

    Add JUnit dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,25 @@ plugins {
 
 group = "com.ss.sgerkin"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("junit", "junit", "4.12")
+    testImplementation("org.mockito", "mockito-junit-jupiter", "3.3.3")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        events("passed", "failed", "skipped")
+    }
 }
